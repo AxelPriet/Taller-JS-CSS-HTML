727a8561fbb0842c9a536307a182b06981752314
XxCamilo14xX <142549722+XxCamilo14xX@users.noreply.github.com>
2023-10-28 23:08:14 -0500

    Update appCT.js

--- ejercicios JS/contador/appCT.js
+++ ejercicios JS/contador/appCT.js
@@ -1,5 +1,6 @@
 function contarCaracteres() {
     var texto = document.getElementById("texto").value;
-    var longitud = texto.length;
-    document.getElementById("resultado").innerHTML = "Cantidad de caracteres de el texto ingresado: " + longitud;
+    var textoSinEspacios = texto.replace(/\s+/g, '');
+    var longitud = textoSinEspacios.length;
+    document.getElementById("resultado").innerHTML = "Cantidad de caracteres del texto ingresado:" + longitud;
 }
